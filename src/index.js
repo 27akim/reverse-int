a1aa9180536c6253f7f2cf83c36b311c80ba8444
27akim <criminal.ok1@gmail.com>
2021-03-04 12:09:51 +0300

    feat: add js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    let str = n.toString().split('');
+    let resultStr = '';
+    for(let i = str.length -1 ; i >= 0; i--)
+    {
+        resultStr += str[i];
+    }
+    return parseInt(resultStr);
 }
